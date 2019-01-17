fd549bc23d15c293f3412ef226df67a6b0ada15b
root <858080796@qq.com>
2019-01-17 14:29:34 +0800

    modified File_del_tar.sh add Multithreaded shell  多线程

--- File_del_tar/File_del_tar.sh
+++ File_del_tar/File_del_tar.sh
@@ -27,18 +27,22 @@ function F_TAR_FILE () {
 	#	tar 归档成功 就删除原文件
 	#tar czvf $TAR_NAME.$(date --date="-1 day" +%Y%m%d).tar.gz $(find -mindepth 2 -mtime 1 ) && find -mindepth 2 -mtime 1 -exec rm -rf {} \;
 	#	tar 归档成功与否 都删除原文件
-	tar czvf $TAR_NAME.$(date --date="-1 day" +%Y%m%d).tar.gz $(find -mindepth 2 -mtime 1 ) ; find -mindepth 2 -mtime 1 -exec rm -rf {} \;
+	tar czvf $TAR_NAME.$(date --date="-1 day" +%Y%m%d).tar.gz $(find -mindepth 2 -mtime 0 ) ; find -mindepth 2 -mtime 0 -exec rm -rf {} \;
 }
 
 #	主程序
 function F_NEED_FIND_DIR () {
 	DIR_PATH=$1 FILE_KEYNAME=$2 DAYS_AGO=$3
 	for i in $(echo $1) ;do
+		{
 		echo "...cd $i.."
 		cd $i
 		[ -z $FILE_KEYNAME ] &&  F_DEL_FILE $i "" $DAYS_AGO ||  F_DEL_FILE $i $FILE_KEYNAME $DAYS_AGO
-		F_TAR_FILE $i 
-	done 
+		F_TAR_FILE $i
+		}& 
+		# {}把循环体括起来，后面加&符号，表示每次循环都把命令放入后台运行，一旦放入后台，意味着{}里面的命令交给了操作系统的一个线程处理，实现shell多线程
+	done
+	wait	#等待（wait命令）上面的命令（放入后台的）都执行完毕了再往下执行 
 }
 
 #范例
