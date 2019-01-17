6d5b4c2b7a8a29bfa36383fc0f031bc01cd13863
root <858080796@qq.com>
2019-01-17 11:15:40 +0800

    modified File_del_tar.sh ,add README.md

--- File_del_tar/File_del_tar.sh
+++ File_del_tar/File_del_tar.sh
@@ -22,15 +22,20 @@ function F_DEL_FILE () {
 function F_TAR_FILE () {
 	DIR_PATH=$1
 	TAR_NAME=$(basename $DIR_PATH)
-	tar czvf $TAR_NAME.$(date --date="-1 day" +%Y%m%d).tar.gz $(find $DIR_PATH -mindepth 2 -mtime -2 -mtime +0 ) && find $DIR_PATH -mindepth 2 -mtime -2 -mtime +0 -exec rm -rf {} \;
+	#tar czvf $TAR_NAME.$(date --date="-1 day" +%Y%m%d).tar.gz $(find $DIR_PATH -mindepth 2 -mtime -2 -mtime +0 ) && find $DIR_PATH -mindepth 2 -mtime -2 -mtime +0 -exec rm -rf {} \;
+	#	二选一
+	#	tar 归档成功 就删除原文件
+	#tar czvf $TAR_NAME.$(date --date="-1 day" +%Y%m%d).tar.gz $(find -mindepth 2 -mtime 6 ) && find -mindepth 2 -mtime 6 -exec rm -rf {} \;
+	#	tar 归档成功与否 都删除原文件
+	tar czvf $TAR_NAME.$(date --date="-1 day" +%Y%m%d).tar.gz $(find -mindepth 2 -mtime 6 ) ; find -mindepth 2 -mtime 6 -exec rm -rf {} \;
 }
 
 #	主程序
 function F_NEED_FIND_DIR () {
 	DIR_PATH=$1 FILE_KEYNAME=$2 DAYS_AGO=$3
 	for i in $(echo $1) ;do
-		echo $i
-		cd $DIR_PATH
+		echo "...cd $i.."
+		cd $i
 		[ -z $FILE_KEYNAME ] &&  F_DEL_FILE $i "" $DAYS_AGO ||  F_DEL_FILE $i $FILE_KEYNAME $DAYS_AGO
 		F_TAR_FILE $i 
 	done 
@@ -42,7 +47,7 @@ function F_NEED_FIND_DIR () {
 F_NEED_FIND_DIR "/data/logs/td-charge" "" "3" 
 
 # 	删除/data/logs/td-zuul 和 /data/logs/td-cube 两目录下 第7天以前的文件，并tar归档前一天文件
-F_NEED_FIND_DIR "/data/logs/td-zuul /data/logs/td-cube" "" "6" 
+F_NEED_FIND_DIR "/data/logs/td-third /data/logs/td-cube" "" "6" 
 
 # 	删除/data/logs/td-charge 和 /data/logs/td-client 两目录下 第7天以前且文件名匹配1800的文件，并tar归档前一天文件
 F_NEED_FIND_DIR "/data/logs/td-admin /data/logs/td-client" "*1800*" "6" 
---
+++ File_del_tar/README.md
@@ -0,0 +1,2 @@
+# File_del_tar
+ Delete files from some days ago,tar compress files from some days ago 
